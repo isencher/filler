5124346cb5d7dd12088b5cafac2d841ec7584f05
sencher <sencher@163.com>
2024-03-19 15:50:43 +0800

    alter modules as relative reference in __init__.py of _util

--- src/filter/_utils/__init__.py
+++ src/filter/_utils/__init__.py
@@ -1,5 +1,6 @@
-from _path import create_output_dir, create_output_path
-from _utils import is_number, rand_float
+from ._path import create_output_dir, create_output_path
+from ._utils import is_number, rand_float
+from .__fill import fill_docx, fill_xlsx
 
 __all__ = [
     "create_output_dir",
@@ -7,4 +8,7 @@ __all__ = [
     
     "is_number",
     "rand_float",
+
+    "fill_docx",
+    "fill_xlsx",
 ]
