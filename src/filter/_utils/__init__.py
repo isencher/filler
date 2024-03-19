0849cbd757a1414c98f693444f77ca164453c26e
sencher <sencher@163.com>
2024-03-19 14:48:48 +0800

    encapsulation api of _util package by __init__.py with 4 funcs

---
+++ src/filter/_utils/__init__.py
@@ -0,0 +1,10 @@
+from _path import create_output_dir, create_output_path
+from _utils import is_number, rand_float
+
+__all__ = [
+    "create_output_dir",
+    "create_output_path",
+    
+    "is_number",
+    "rand_float",
+]
