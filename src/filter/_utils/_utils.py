dbc54c3e8fa7415865449e09f0869e357d4ba625
sencher <sencher@163.com>
2024-03-19 14:32:23 +0800

    create _utils.py with is_number & rand_float funcs

---
+++ src/filter/_utils/_utils.py
@@ -0,0 +1,44 @@
+import random
+
+__all__ = [
+    "is_number",
+    "rand_float",
+]
+
+def is_number(str:str)->bool:
+    '''
+    Determine whether a string is a number, and return True if it is a number, else False
+    str: str, a string that needs to be determined whether it is a number 
+
+    >>> is_number("345")
+    True
+    >>> is_number("345.33")
+    True
+    >>> is_number("abc")
+    False
+
+    '''
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+def rand_float(start:float=-1, end:float=1, point:int=2)->float:
+    '''
+    generate a random floating-point number between start and end.
+    param start: float, lower boundary.
+    param end: float, higher boundary.
+    param point: int, decimal places.
+    return : float, random float between start and end.
+    ''' 
+
+    return round(
+        random.uniform(start, end),
+        point
+    )
+
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
