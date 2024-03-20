28f55ea97c475098e91368f57fe92d29b002f4f1
sencher <sencher@163.com>
2024-03-20 22:24:49 +0800

    add _error.py in _types with  some ParamError objects

---
+++ src/filter/_types/_error.py
@@ -0,0 +1,64 @@
+__all__ = [
+    "FillerError",
+    "ParamError",
+    "FillDataCollectionTypeError",
+    "FillDataCollectionEmptyError",
+    "FillTemplateTypeError",
+    "FillOutputDirError",
+]
+
+class FillerError(Exception):
+    pass
+
+class ParamError(FillerError):
+    pass
+
+class FillDataCollectionError(ParamError):
+    pass
+
+class FillDataCollectionTypeError(FillDataCollectionError):
+    '''
+    Exception raised for errors in the input.
+    
+    Attributes:
+        message -- explanation of the error
+    '''
+    def __init__(self, message="It should be either a DataFrame or a dict with keys as strings and values as Series or dicts!"):
+        self.message = message
+        super().__init__(self.message)
+        
+class FillDataCollectionEmptyError(FillDataCollectionError):
+    '''
+    Exception raised for errors in the input.
+    
+    Attributes:
+        message -- explanation of the error
+    '''
+    def __init__(self, message="It should not be empty!"):
+        self.message = message
+        super().__init__(self.message)
+               
+class FillTemplateError(ParamError):
+    pass
+
+class FillTemplateTypeError(FillTemplateError):
+    '''
+    Exception raised for errors in the input.
+    
+    Attributes:
+        message -- explanation of the error
+    '''
+    def __init__(self, message="It should be a exist file path with docx or xlsx extension!"):
+        self.message = message
+        super().__init__(self.message)
+        
+class FillOutputDirError(ParamError):
+    '''
+    Exception raised for errors in the input.
+    
+    Attributes:
+        message -- explanation of the error
+    '''
+    def __init__(self, message="It should be a exist directory!"):
+        self.message = message
+        super().__init__(self.message)  
